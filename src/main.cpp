0970f1ccd1ceeb17fd0bc5ada667df57f3404298
Quang Ho <hoqm01@pfw.edu>
2026-02-01 14:14:26 -0500

    feat: Integrate the raylib library, its examples, and associated build system files into the project.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "raylib.h"
+
+int main() {
+    InitWindow(800, 600, "My Awesome Game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Raylib + CMake Ready!", 250, 280, 30, DARKGRAY);
+        DrawRectangle(375, 350, 50, 50, RED);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
